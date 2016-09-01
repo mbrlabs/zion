5088bb9c0032c3ef61a1de7d1014f446b4ff45d4
Marcus Brummer <mbrlabs7@gmail.com>
2016-09-01 19:14:45 +0200

    Documentation

--- context.go
+++ context.go
@@ -17,16 +17,13 @@ package zion
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"mime"
 	"net/http"
 	"os"
-	"strconv"
 )
 
-// extra keys
+// Extra keys
 const (
-	ExtraUser = "extra_user"
+	ExtraUser = "github.com/mbrlabs/zion/security:extra_user"
 )
 
 // Context
@@ -43,7 +40,7 @@ type Context struct {
 	extras    map[string]interface{}
 }
 
-// NewContext #TODO
+// NewContext creates a new context
 func NewContext(z *Zion, w http.ResponseWriter, r *http.Request) *Context {
 	return &Context{
 		zion:      z,
@@ -54,15 +51,17 @@ func NewContext(z *Zion, w http.ResponseWriter, r *http.Request) *Context {
 	}
 }
 
-// Render #TODO
+// Render renders a template given the template name and some data
 func (ctx *Context) Render(name string, data interface{}) {
 	ctx.zion.config.TemplateEngine.Render(name, data, ctx.writer)
 }
 
-func (ctx *Context) Html(html string) {
-	fmt.Fprintf(ctx.writer, "%s", html)
+// String simply sends the given string to the client
+func (ctx *Context) String(str string) {
+	fmt.Fprintf(ctx.writer, "%s", str)
 }
 
+// Json coverts the data to a json, sets the content type & sends it to the client
 func (ctx *Context) Json(data interface{}) {
 	data, err := json.Marshal(data)
 	if err != nil {
@@ -72,6 +71,8 @@ func (ctx *Context) Json(data interface{}) {
 	fmt.Fprintf(ctx.writer, "%s", data)
 }
 
+// File sends a file to the client. This method implements the If-Modified-Since/Last-Modified headers
+// and sends files only if the client has an older version of the file.
 func (ctx *Context) File(path string) {
 	// open file
 	file, err := os.Open(path)
@@ -94,54 +95,67 @@ func (ctx *Context) File(path string) {
 	http.ServeContent(ctx.writer, ctx.request, fileInfo.Name(), fileInfo.ModTime(), file)
 }
 
+// SendStatus replies only with a status response
 func (ctx *Context) SendStatus(status int) {
 	ctx.writer.WriteHeader(status)
 }
 
+// Redirect redirects the user to the given relative url
 func (ctx *Context) Redirect(path string) {
 	http.Redirect(ctx.writer, ctx.request, path, http.StatusTemporaryRedirect)
 }
 
+// Method returns the http method of th request
 func (ctx *Context) Method() string {
 	return ctx.request.Method
 }
 
+// RequestHeader returns the http header of the request
 func (ctx *Context) RequestHeader() http.Header {
 	return ctx.request.Header
 }
 
+// ResponseHeader returns the http header of the response
 func (ctx *Context) ResponseHeader() http.Header {
 	return ctx.writer.Header()
 }
 
+// Path returns path of the request
 func (ctx *Context) Path() string {
 	return ctx.request.URL.Path
 }
 
+// URLParams returnes the url parameters (/home/:param/*file), parsed by the router
 func (ctx *Context) URLParams() map[string]string {
 	return ctx.urlParams
 }
 
+// URLParam returns one url parameter by name
 func (ctx *Context) URLParam(name string) string {
 	return ctx.urlParams[name]
 }
 
+// FormValue returns one url encoded post parameter by name
 func (ctx *Context) FormValue(name string) string {
 	return ctx.request.FormValue(name)
 }
 
+// Cookie gets a cookie in the request by name
 func (ctx *Context) Cookie(name string) (*http.Cookie, error) {
 	return ctx.request.Cookie(name)
 }
 
+// SetCookie sets a cookie for the response
 func (ctx *Context) SetCookie(c *http.Cookie) {
 	http.SetCookie(ctx.writer, c)
 }
 
+// Extra returns an user defined extra
 func (ctx *Context) Extra(key string) interface{} {
 	return ctx.extras[key]
 }
 
+// AddExtra adds a user defined extra
 func (ctx *Context) AddExtra(key string, extra interface{}) {
 	ctx.extras[key] = extra
 }
