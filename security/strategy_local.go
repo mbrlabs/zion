969c11789869d67b4aaf78390aea188167480080
Marcus Brummer <mbrlabs7@gmail.com>
2016-09-03 16:09:40 +0200

    Added custom success/error/logout handlers to the local security strategy

--- security/strategy_local.go
+++ security/strategy_local.go
@@ -24,13 +24,18 @@ import (
 //------------------------------------------------------------------------------------
 
 type LocalSecurityStrategy struct {
-	userStore       UserStore
-	sessionStore    SessionStore
-	successRedirect string
-	failureRedirect string
+	userStore      UserStore
+	sessionStore   SessionStore
+	loginNameField string
+	passwordField  string
+
 	logoutRedirect  string
-	loginNameField  string
-	passwordField   string
+	failureRedirect string
+	successRedirect string
+
+	logoutHandler  zion.HandlerFunc
+	failureHandler zion.HandlerFunc
+	successHandler zion.HandlerFunc
 }
 
 func NewLocalSecurityStrategy(userStore UserStore, sessionStore SessionStore) *LocalSecurityStrategy {
@@ -54,9 +59,41 @@ func (ls *LocalSecurityStrategy) SetRedirects(successRedirect string, failureRed
 	ls.logoutRedirect = logoutRedirect
 }
 
+func (ls *LocalSecurityStrategy) SetSuccessHandler(onSuccess zion.HandlerFunc) {
+	ls.successHandler = onSuccess
+}
+
+func (ls *LocalSecurityStrategy) SetFailureHandler(onFailure zion.HandlerFunc) {
+	ls.failureHandler = onFailure
+}
+
+func (ls *LocalSecurityStrategy) SetLogoutHandler(onLogout zion.HandlerFunc) {
+	ls.logoutHandler = onLogout
+}
+
 func (ls *LocalSecurityStrategy) SetPostParameterFields(loginNameField string, passwordField string) {
 	ls.loginNameField = loginNameField
 	ls.passwordField = passwordField
+}
+
+func (ls *LocalSecurityStrategy) handleLoginError(ctx *zion.Context) {
+	if ls.failureHandler != nil {
+		ls.failureHandler(ctx)
+	} else if len(ls.failureRedirect) > 0 {
+		ctx.Redirect(ls.failureRedirect)
+	} else {
+		ctx.SendStatus(http.StatusBadRequest)
+	}
+}
+
+func (ls *LocalSecurityStrategy) handleLoginSuccess(ctx *zion.Context) {
+	if ls.successHandler != nil {
+		ls.successHandler(ctx)
+	} else if len(ls.successRedirect) > 0 {
+		ctx.Redirect(ls.successRedirect)
+	} else {
+		ctx.SendStatus(http.StatusOK)
+	}
 }
 
 func (ls *LocalSecurityStrategy) Authenticate() zion.HandlerFunc {
@@ -66,14 +103,14 @@ func (ls *LocalSecurityStrategy) Authenticate() zion.HandlerFunc {
 
 		// handle empty input
 		if len(login) == 0 || len(password) == 0 {
-			ctx.Redirect(ls.failureRedirect)
+			ls.handleLoginError(ctx)
 			return
 		}
 
 		// get user
 		user, err := ls.userStore.GetUserByLogin(login)
 		if user == nil || err != nil {
-			ctx.Redirect(ls.failureRedirect)
+			ls.handleLoginError(ctx)
 			return
 		}
 
@@ -90,22 +127,13 @@ func (ls *LocalSecurityStrategy) Authenticate() zion.HandlerFunc {
 					Expires: session.Expire,
 				}
 				ctx.SetCookie(cookie)
-				// redirect to succcess page
-				if len(ls.successRedirect) > 0 {
-					ctx.Redirect(ls.successRedirect)
-				} else {
-					ctx.SendStatus(http.StatusOK)
-				}
+				ctx.AddExtra(zion.ExtraUser, user)
+				ls.handleLoginSuccess(ctx)
 				return
 			}
 		}
 
-		// redirect to error page
-		if len(ls.failureRedirect) > 0 {
-			ctx.Redirect(ls.failureRedirect)
-		} else {
-			ctx.SendStatus(http.StatusBadRequest)
-		}
+		ls.handleLoginError(ctx)
 	}
 }
 
